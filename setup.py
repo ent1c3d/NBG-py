8a73ed21699c5c4d71c9480e62a6424c92352e06
ent1c3d <ent1c3d@gmail.com>
2017-10-13 02:03:36 +0400

    no message

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='nbg-py',
       version='0.1',
       description='Client for National Bank of Georgia currency api',
-      url='',
+      url='https://github.com/ent1c3d/NBG-py',
       author='Goga Patarkatsishvili',
-      author_email='',
+      author_email='ent1c3d@gmail.com',
       license='MIT',
       packages=['nbg'],
       zip_safe=False)
